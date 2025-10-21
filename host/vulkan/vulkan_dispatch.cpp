285a7db5d2127144ea2c9ec6fc155974ab08073c
Serdar Kocdemir <kocdemir@google.com>
2025-10-21 16:17:01 +0100

    Enable VVL usage on moltenvk
    With the workarounds removed and binaries updated, should be safe
    to use VVL on macOS for vulkan debugging.
    
    Bug: 351765838
    Test: run emulator with vvl on mac
    Change-Id: Icf87e5af26c2c36d067841e598abf8fe15a75b94

--- host/vulkan/vulkan_dispatch.cpp
+++ host/vulkan/vulkan_dispatch.cpp
@@ -141,14 +141,6 @@ static void initIcdPaths(bool forTesting) {
 
             // MVK_CONFIG_USE_MTLHEAP is required for VK_EXT_external_memory_metal
             gfxstream::base::setEnvironmentVariable("MVK_CONFIG_USE_MTLHEAP", "1");
-
-            // TODO(b/351765838): VVL won't work with MoltenVK due to the current
-            //  way of external memory handling, add it into disable list to
-            //  avoid users enabling it implicitly (i.e. via vkconfig).
-            //  It can be enabled with VK_LOADER_LAYERS_ALLOW=VK_LAYER_KHRONOS_validation
-            GFXSTREAM_INFO("Vulkan Validation Layers won't be enabled with MoltenVK");
-            gfxstream::base::setEnvironmentVariable("VK_LOADER_LAYERS_DISABLE",
-                                                    "VK_LAYER_KHRONOS_validation");
         }
 #else
         // By default, on other platforms, just use whatever the system
